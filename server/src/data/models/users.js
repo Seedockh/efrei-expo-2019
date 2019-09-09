dd1afffdd965b598209d2dada224cb0807d7253a
Seedockh <pierre.herisse@gmail.com>
2019-09-09 10:44:53 +0200

    Init db, users table and dependencies

---
+++ server/src/data/models/users.js
@@ -0,0 +1,47 @@
+import Sequelize, { Model } from "sequelize";
+
+export default class Users extends Model {
+  static init(database){
+
+    return super.init({
+
+      firstname: {
+        type: Sequelize.STRING,
+      },
+
+      lastname: {
+        type: Sequelize.STRING,
+      },
+
+      city: {
+        type: Sequelize.STRING,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE(3),
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+      }
+
+    }, {
+      tableName: "users",
+      sequelize: database,
+
+      indexes: [
+        {
+          unique: true,
+          fields: ["firstname"]
+        }
+      ],
+    })
+
+  };
+
+  toJSON() {
+    return Object.assign({}, this.get());
+  }
+}
