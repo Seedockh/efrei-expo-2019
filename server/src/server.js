4831ff96fb7a4b6d635e3135334d394bb3a44641
Seedockh <pierre.herisse@gmail.com>
2019-09-09 13:19:25 +0200

    fix favicon crash

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,11 @@
+import iconv from 'iconv-lite';
+import encodings from 'iconv-lite/encodings';
 import { GraphQLServer } from 'graphql-yoga'
 import schema from './schema'
 import { db } from './data/initdb'
 const PORT = 4000
+
+iconv.encodings = encodings;
 
 db.sync({ force: false });
 
