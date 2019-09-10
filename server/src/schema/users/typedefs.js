74cb80c1f8f1ffcda46a0c9ce7c0ea293a7ad58b
Seedockh <pierre.herisse@gmail.com>
2019-09-10 16:16:50 +0200

    Add Login Query for firstname:lastname

--- server/src/schema/users/typedefs.js
+++ server/src/schema/users/typedefs.js
@@ -10,15 +10,21 @@ const typeDefs = `
   type Query {
     users: [Users!]!
     user(id: ID!): Users!
+    login(data: LoginInput!): Users!
   }
 
   type Mutation {
-    editUser(id: ID!, data: EditUserInput!): Users!
     createUser(data: EditUserInput!): Users!
+    editUser(id: ID!, data: EditUserInput!): Users!
     deleteUser(id: ID!): ResultMessage!
   }
 
   # ---
+
+  input LoginInput {
+    firstname: String
+    lastname: String
+  }
 
   input EditUserInput {
     firstname: String
